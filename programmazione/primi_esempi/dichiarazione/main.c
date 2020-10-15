28a4c7279d0c3ed412cd8565ac5b4e6844b00150
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-10-15 17:48:47 +0200

    Primo esempio con dichirazione di variabile

---
+++ programmazione/primi_esempi/dichiarazione/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    a = 7;
+    int b, c, d;
+    double f;
+    b = 13;
+    //NO, eventualmente anni_trascorsi
+    //int anni trascorsi;
+    //Attenzione ai tipi nelle operazioni aritmetiche
+    f = a / 2.0;
+    //Forme contratte
+    a += b; //a = a + b; ADD R0, R0, R1
+    c++; //c = c + 1;
+    return 0;
+}
