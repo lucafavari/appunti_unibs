028f0365e70321cba0c1315f2d9281244f082291
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-11-14 09:31:43 +0100

    Aggiunge la ricerca binaria

--- programmazione/vettori/operazioni_elementari/main.c
+++ programmazione/vettori/operazioni_elementari/main.c
@@ -24,6 +24,23 @@ int ricerca_esaustiva(int v[], int n, int cercato)
     return -1;
 }
 
+int ricerca_binaria(int v[], int n, int cercato)
+{
+    int a = 0, b = n - 1;
+    int m = (a + b)/2;
+    while(a <= b)
+    {
+        if (v[m] == cercato)
+            return m;
+        else if (v[m] < cercato)
+            a = m + 1;
+        else
+            b = m - 1;
+        m = (a + b)/2;
+    }
+    return -1;
+}
+
 void inserimento_inizio(int v[], int n, int valore)
 {
     for (int i = n; i > 0; i--)
@@ -31,15 +48,57 @@ void inserimento_inizio(int v[], int n, int valore)
     v[0] = valore;
 }
 
-void inserimento_posizione(int v[], int n, int valore, int pos)
+int inserimento_posizione(int v[], int n, int valore, int pos)
 {
     if (pos < 0 || pos > n)
-        return;
+        return -1;
     for (int i = n; i > pos; i--)
         v[i] = v[i - 1];
     v[pos] = valore;
+    return pos;
 }
 
+int elimina_posizione(int v[], int n, int pos)
+{
+    if (pos < 0 || pos >= n)
+        return -1;
+    for (int i = pos; i < n - 1; i++)
+        v[i] = v[i + 1];
+    //return 1;
+    //return pos;
+    return n - 1;
+}
+
+void bubble_sort(int v[], int n)
+{
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = n - 1; j > i; j--)
+            if (v[j - 1] > v[j])
+            {
+                int temp = v[j - 1];
+                v[j - 1] = v[j];
+                v[j] = temp;
+            }
+    }
+
+}
+
+void selection_sort(int v[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int pos_minimo = i;
+        for (int j = i; j < n; j++)
+            if (v[j] < v[pos_minimo])
+                pos_minimo = j;
+        int temp = v[i];
+        v[i] = v[pos_minimo];
+        v[pos_minimo] = temp;
+    }
+
+}
 
 int main()
 {
@@ -72,5 +131,32 @@ int main()
     inserimento_posizione(v, elementi, 999999, 7);
     elementi++;
     stampa_vettore(v, elementi);
+
+    elimina_posizione(v, elementi, 3);
+    elementi--;
+    stampa_vettore(v, elementi);
+
+    printf("Bubblesort\n");
+    bubble_sort(v, elementi);
+    stampa_vettore(v, elementi);
+
+    printf("Selectionsort\n");
+    riempi_casuale(v, elementi);
+    stampa_vettore(v, elementi);
+    selection_sort(v,elementi);
+    stampa_vettore(v, elementi);
+
+    int d = ricerca_binaria(v, elementi, 10);
+    if (d == -1)
+        printf("Non trovato\n");
+    else
+        printf("Trovato in posizione %d\n", d);
+
+    d = ricerca_binaria(v, elementi, 6270);
+    if (d == -1)
+        printf("Non trovato\n");
+    else
+        printf("Trovato in posizione %d\n", d);
+
     return 0;
 }
