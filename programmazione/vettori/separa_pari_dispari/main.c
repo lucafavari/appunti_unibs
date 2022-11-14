a57e04c71ceb05c00e3ec8621ff19438293536c6
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2022-11-14 11:12:11 +0100

    Aggiorna l'esempio della separazione pari-dispari

--- programmazione/vettori/separa_pari_dispari/main.c
+++ programmazione/vettori/separa_pari_dispari/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
 
 int main() {
-
+    int valori[MAX];
+    int N;
+    printf("Quanti valori vuoi inserire (<= 100): ");
+    scanf("%d", &N);
+    for (int i = 0; i < N; ++i) {
+        //printf("Inserisci un valore: ");
+        //scanf("%d", &valori[i]);
+        valori[i] = rand()%100;
+    }
+    for (int i = 0; i < N; ++i) {
+        if (valori[i] % 2 == 0){
+            printf("%d\n", valori[i]);
+        }
+    }
+    for (int i = 0; i < N; ++i) {
+        if (valori[i] % 2 == 1){
+            printf("%d\n", valori[i]);
+        }
+    }
     return 0;
 }
